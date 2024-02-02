fcc61bda405c61abd008b7503a4281e055d98376
Lukas <lhassel@kexxu.com>
2024-02-02 09:34:17 +0100

    Kexxu adaptations

--- demo.py
+++ demo.py
@@ -18,17 +18,51 @@ from vitpose_model import ViTPoseModel
 import json
 from typing import Dict, Optional
 
+import requests
+
+class KexxuIterator:
+    def __init__(self, collection, callback_url):
+        self.collection = collection
+        self.length = len(collection)
+        self.callback_url = callback_url
+        self.last_callback_percentage = 0
+        self.current = 0
+
+    def __iter__(self):
+        self.current = 0
+        return self
+    
+    def percentage(self):
+        return int((self.current / self.length) * 80) + 10
+
+    def make_callback(self):
+        requests.post(self.callback_url, json={'status':'processing', 'message':'Creating segments...', 'progress_perc':self.percentage()})
+
+    def __next__(self):
+        if self.current < self.length:
+            print("NOW AT", self.percentage(), "%")
+            if self.last_callback_percentage < self.percentage():
+                self.make_callback()
+                self.last_callback_percentage = self.percentage()
+            result = self.collection[self.current]
+            self.current += 1
+            return result
+        else:
+            raise StopIteration
+
 def main():
     parser = argparse.ArgumentParser(description='HaMeR demo code')
     parser.add_argument('--checkpoint', type=str, default=DEFAULT_CHECKPOINT, help='Path to pretrained model checkpoint')
     parser.add_argument('--img_folder', type=str, default='images', help='Folder with input images')
     parser.add_argument('--out_folder', type=str, default='out_demo', help='Output folder to save rendered results')
+    parser.add_argument('--bbox_file', type=str, default='bbox_file.json', help='Output file to safe the detected hand bboxes')
     parser.add_argument('--side_view', dest='side_view', action='store_true', default=False, help='If set, render side view also')
     parser.add_argument('--full_frame', dest='full_frame', action='store_true', default=True, help='If set, render all people together also')
     parser.add_argument('--save_mesh', dest='save_mesh', action='store_true', default=False, help='If set, save meshes to disk also')
     parser.add_argument('--batch_size', type=int, default=1, help='Batch size for inference/fitting')
     parser.add_argument('--rescale_factor', type=float, default=2.0, help='Factor for padding the bbox')
     parser.add_argument('--file_type', nargs='+', default=['*.jpg', '*.png'], help='List of file extensions to consider')
+    parser.add_argument('--callback_url', type=str, help='URL that will be posted to, to keep users informed about the progress')
 
     args = parser.parse_args()
 
@@ -64,8 +98,11 @@ def main():
     # Get all demo images ends with .jpg or .png
     img_paths = [img for end in args.file_type for img in Path(args.img_folder).glob(end)]
 
+    # for storing the detected bounding boxes in each frame
+    bboxesAtFrame = {}
+
     # Iterate over all images in folder
-    for img_path in img_paths:
+    for img_path in KexxuIterator(img_paths, args.callback_url):
         img_cv2 = cv2.imread(str(img_path))
 
         # Detect humans in image
@@ -85,6 +122,7 @@ def main():
 
         bboxes = []
         is_right = []
+        estim_prob = []
 
         # Use hands based on hand keypoint detections
         for vitposes in vitposes_out:
@@ -98,102 +136,129 @@ def main():
                 bbox = [keyp[valid,0].min(), keyp[valid,1].min(), keyp[valid,0].max(), keyp[valid,1].max()]
                 bboxes.append(bbox)
                 is_right.append(0)
+                estim_prob.append(sum(keyp[:,2])/21)
+
             keyp = right_hand_keyp
             valid = keyp[:,2] > 0.5
             if sum(valid) > 3:
                 bbox = [keyp[valid,0].min(), keyp[valid,1].min(), keyp[valid,0].max(), keyp[valid,1].max()]
                 bboxes.append(bbox)
                 is_right.append(1)
+                estim_prob.append(sum(keyp[:,2])/21)
 
-        if len(bboxes) == 0:
-            continue
-
-        boxes = np.stack(bboxes)
-        right = np.stack(is_right)
-
-        # Run reconstruction on all detected hands
-        dataset = ViTDetDataset(model_cfg, img_cv2, boxes, right, rescale_factor=args.rescale_factor)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=0)
-
-        all_verts = []
-        all_cam_t = []
-        all_right = []
+        print(img_path, bboxes, is_right)
         
-        for batch in dataloader:
-            batch = recursive_to(batch, device)
-            with torch.no_grad():
-                out = model(batch)
+        bboxesAtFrame[img_path] = []
+        for (bbox, right_hand, p) in zip(bboxes, is_right, estim_prob):
+            p = float(p)
+            x1,y1,x2,y2 = bbox
+            x1 = float(x1)
+            x2 = float(x2)
+            y1 = float(y1)
+            y2 = float(y2)
+            bucket = bboxesAtFrame.get(img_path, [])
+            bucket.append([x1, y1, x2, y2, p, 0 if right_hand else 1])
+            bboxesAtFrame[img_path] = bucket
 
-            multiplier = (2*batch['right']-1)
-            pred_cam = out['pred_cam']
-            pred_cam[:,1] = multiplier*pred_cam[:,1]
-            box_center = batch["box_center"].float()
-            box_size = batch["box_size"].float()
-            img_size = batch["img_size"].float()
-            multiplier = (2*batch['right']-1)
-            scaled_focal_length = model_cfg.EXTRA.FOCAL_LENGTH / model_cfg.MODEL.IMAGE_SIZE * img_size.max()
-            pred_cam_t_full = cam_crop_to_full(pred_cam, box_center, box_size, img_size, scaled_focal_length).detach().cpu().numpy()
+        if len(bboxes) > 0:
+        
+            boxes = np.stack(bboxes)
+            right = np.stack(is_right)
 
-            # Render the result
-            batch_size = batch['img'].shape[0]
-            for n in range(batch_size):
-                # Get filename from path img_path
+            # Run reconstruction on all detected hands
+            dataset = ViTDetDataset(model_cfg, img_cv2, boxes, right, rescale_factor=args.rescale_factor)
+            dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=0)
+
+            all_verts = []
+            all_cam_t = []
+            all_right = []
+            
+            for batch in dataloader:
+                batch = recursive_to(batch, device)
+                with torch.no_grad():
+                    out = model(batch)
+
+                multiplier = (2*batch['right']-1)
+                pred_cam = out['pred_cam']
+                pred_cam[:,1] = multiplier*pred_cam[:,1]
+                box_center = batch["box_center"].float()
+                box_size = batch["box_size"].float()
+                img_size = batch["img_size"].float()
+                multiplier = (2*batch['right']-1)
+                scaled_focal_length = model_cfg.EXTRA.FOCAL_LENGTH / model_cfg.MODEL.IMAGE_SIZE * img_size.max()
+                pred_cam_t_full = cam_crop_to_full(pred_cam, box_center, box_size, img_size, scaled_focal_length).detach().cpu().numpy()
+
                 img_fn, _ = os.path.splitext(os.path.basename(img_path))
-                person_id = int(batch['personid'][n])
-                white_img = (torch.ones_like(batch['img'][n]).cpu() - DEFAULT_MEAN[:,None,None]/255) / (DEFAULT_STD[:,None,None]/255)
-                input_patch = batch['img'][n].cpu() * (DEFAULT_STD[:,None,None]/255) + (DEFAULT_MEAN[:,None,None]/255)
-                input_patch = input_patch.permute(1,2,0).numpy()
+                # Render the result
+                batch_size = batch['img'].shape[0]
+                for n in range(batch_size):
+                    # Get filename from path img_path
+                    person_id = int(batch['personid'][n])
+                    # white_img = (torch.ones_like(batch['img'][n]).cpu() - DEFAULT_MEAN[:,None,None]/255) / (DEFAULT_STD[:,None,None]/255)
+                    input_patch = batch['img'][n].cpu() * (DEFAULT_STD[:,None,None]/255) + (DEFAULT_MEAN[:,None,None]/255)
+                    input_patch = input_patch.permute(1,2,0).numpy()
 
-                regression_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
-                                        out['pred_cam_t'][n].detach().cpu().numpy(),
-                                        batch['img'][n],
-                                        mesh_base_color=LIGHT_BLUE,
-                                        scene_bg_color=(1, 1, 1),
-                                        )
+                    # regression_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
+                    #                         out['pred_cam_t'][n].detach().cpu().numpy(),
+                    #                         batch['img'][n],
+                    #                         mesh_base_color=LIGHT_BLUE,
+                    #                         scene_bg_color=(1, 1, 1),
+                    #                         )
 
-                if args.side_view:
-                    side_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
-                                            out['pred_cam_t'][n].detach().cpu().numpy(),
-                                            white_img,
-                                            mesh_base_color=LIGHT_BLUE,
-                                            scene_bg_color=(1, 1, 1),
-                                            side_view=True)
-                    final_img = np.concatenate([input_patch, regression_img, side_img], axis=1)
-                else:
-                    final_img = np.concatenate([input_patch, regression_img], axis=1)
+                    # if args.side_view:
+                    #     side_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
+                    #                             out['pred_cam_t'][n].detach().cpu().numpy(),
+                    #                             white_img,
+                    #                             mesh_base_color=LIGHT_BLUE,
+                    #                             scene_bg_color=(1, 1, 1),
+                    #                             side_view=True)
+                    #     final_img = np.concatenate([input_patch, regression_img, side_img], axis=1)
+                    # else:
+                    #     final_img = np.concatenate([input_patch, regression_img], axis=1)
 
-                cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}_{person_id}.png'), 255*final_img[:, :, ::-1])
+                    # cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}_{person_id}.png'), 255*final_img[:, :, ::-1])
 
-                # Add all verts and cams to list
-                verts = out['pred_vertices'][n].detach().cpu().numpy()
-                is_right = batch['right'][n].cpu().numpy()
-                verts[:,0] = (2*is_right-1)*verts[:,0]
-                cam_t = pred_cam_t_full[n]
-                all_verts.append(verts)
-                all_cam_t.append(cam_t)
-                all_right.append(is_right)
+                    # Add all verts and cams to list
+                    verts = out['pred_vertices'][n].detach().cpu().numpy()
+                    is_right = batch['right'][n].cpu().numpy()
+                    verts[:,0] = (2*is_right-1)*verts[:,0]
+                    cam_t = pred_cam_t_full[n]
+                    all_verts.append(verts)
+                    all_cam_t.append(cam_t)
+                    all_right.append(is_right)
 
-                # Save all meshes to disk
-                if args.save_mesh:
-                    camera_translation = cam_t.copy()
-                    tmesh = renderer.vertices_to_trimesh(verts, camera_translation, LIGHT_BLUE, is_right=is_right)
-                    tmesh.export(os.path.join(args.out_folder, f'{img_fn}_{person_id}.obj'))
+                    # Save all meshes to disk
+                    if args.save_mesh:
+                        camera_translation = cam_t.copy()
+                        tmesh = renderer.vertices_to_trimesh(verts, camera_translation, LIGHT_BLUE, is_right=is_right)
+                        tmesh.export(os.path.join(args.out_folder, f'{img_fn}_{person_id}.obj'))
 
-        # Render front view
-        if args.full_frame and len(all_verts) > 0:
-            misc_args = dict(
-                mesh_base_color=LIGHT_BLUE,
-                scene_bg_color=(1, 1, 1),
-                focal_length=scaled_focal_length,
-            )
-            cam_view = renderer.render_rgba_multiple(all_verts, cam_t=all_cam_t, render_res=img_size[n], is_right=all_right, **misc_args)
+            # Render front view
+            if args.full_frame and len(all_verts) > 0:
+                misc_args = dict(
+                    mesh_base_color=LIGHT_BLUE,
+                    scene_bg_color=(1, 1, 1),
+                    focal_length=scaled_focal_length,
+                )
+                cam_view = renderer.render_rgba_multiple(all_verts, cam_t=all_cam_t, render_res=img_size[n], is_right=all_right, **misc_args)
 
-            # Overlay image
-            input_img = img_cv2.astype(np.float32)[:,:,::-1]/255.0
-            input_img = np.concatenate([input_img, np.ones_like(input_img[:,:,:1])], axis=2) # Add alpha channel
-            input_img_overlay = input_img[:,:,:3] * (1-cam_view[:,:,3:]) + cam_view[:,:,:3] * cam_view[:,:,3:]
+                # Overlay image
+                input_img = img_cv2.astype(np.float32)[:,:,::-1]/255.0
+                input_img = np.concatenate([input_img, np.ones_like(input_img[:,:,:1])], axis=2) # Add alpha channel
+                input_img_overlay = input_img[:,:,:3] * (1-cam_view[:,:,3:]) + cam_view[:,:,:3] * cam_view[:,:,3:]
 
-            cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}_all.jpg'), 255*input_img_overlay[:, :, ::-1])
+                cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}_all.jpg'), 255*input_img_overlay[:, :, ::-1])
+        
+        else:
+            # if there were no hands detected, the frame still needs to be saved.
+            img_fn, _ = os.path.splitext(os.path.basename(img_path))
+            cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}_all.jpg'), img_cv2)
+
+    # write out a kexxu-compatible file:
+    with open(args.bbox_file, "w") as file:
+        mock_yolo_output = {"YOLOv5": [bboxesAtFrame[frame] for frame in sorted(bboxesAtFrame.keys())]}
+        json.dump(mock_yolo_output, file)
+        
 
 if __name__ == '__main__':
     main()
